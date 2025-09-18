8542b69f064d35a1bdab776ddbeabe66e1397c2b
fridahnankwanga <fridahnankwanga@gmail.com>
2025-09-19 02:04:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+ // let course_work_mark  = 42;
+//let final_exam_mark =23;
+ //const myName = "Fridah"
+ //let finalMark =course_work_mark /  final_exam_mark;
+ //console.log("The final score  is:"+ finalMark);
+//console.log("When divided it equats to:" + course_work_mark + final_exam_mark)
+const surname ="fridah";
+const other_name="Lalah";
+
+const full_name = surname + other_name;
+console.log(full_name);
