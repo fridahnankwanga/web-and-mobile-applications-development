afda788cd1008a236a9adb170cd6af17e3599c93
fridahnankwanga <fridahnankwanga@gmail.com>
2025-09-29 22:41:25 +0300

    Add files via upload

---
+++ condition.js
@@ -0,0 +1,78 @@
+//condition statements//
+let course_work_mark = 42; 
+let exam_mark = 23;
+let final_mark = course_work_mark + exam_mark;
+
+if (final_mark >= 50) {
+    console.log("You have a retake " + final_mark);
+}
+else {
+    console.log("You have successfully passed " + final_mark);
+}
+
+
+//range
+//if (final_mark<=49){
+   // console.log("Grade F")
+//}
+//else if (final_mark<=59){
+  //  console.log("Grade D")
+//}
+//else if (final_mark<=69){
+   // console.log("Grade C")
+//}
+//else if (final_mark<=79){
+    //console.log("Grade B")
+
+//} if (final_mark <= 100){
+   // console.log("Grade A")
+//} else {
+   // console.log("Invalid mark")
+//}//
+let week_day = 4;
+// 1=Monday, 2=Tuesday, 3=Wednesday, 4=Thursday, 5=Friday, 6=Saturday, 7=Sunday
+//switch (week_day) {
+    //case 1:
+        //console.log("It's Monday");
+       // break;
+   // case 2:
+     //   console.log("It's Tuesday");
+       // break;
+   // case 3:
+     //   console.log("It's Wednesday");
+       // break;
+   // case 4:
+     //   console.log("It's Thursday");
+       // break;
+   // case 5:
+     //   console.log("It's Friday");
+       // break;
+   // case 6:
+     //   console.log("It's Saturday");
+       // break;
+   // case 7:
+     //   console.log("It's Sunday");
+       // break;
+   // default:
+      //  console.log("out of range");
+      //  break
+//}
+//loops
+//let i = 1;
+//while(1 < 5) {
+  //  console.log("The number is " + i);
+   // i++;
+//}
+//let x = 0;
+//while (x < 10) {
+    //console.log("The number is " + x);
+//}
+for (let my_number = 0; my_number <= 10; my_number++) {
+    console.log("my_number ");
+}
+
+
+
+    
+        
+
