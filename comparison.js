3be3e829f9520a83c7cf606b8c4ea8ebcd76cb4d
fridahnankwanga <fridahnankwanga@gmail.com>
2025-10-06 23:56:27 +0300

    Add files via upload

---
+++ comparison.js
@@ -0,0 +1,10 @@
+const a=3,b=2;
+console.log(a>b);
+// same value ,same type
+console.log(5==5);
+// same value,different type
+console.log(5=="5");
+//different values,same type
+console.log("hello" == "Hello");
+    // Justification for using a Condition:
+    // After checking all tasks, we use an `if/else` condition to determine
