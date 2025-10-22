38268aa7f730705660f2ce489ee75d2117e1d61d
fridahnankwanga <fridahnankwanga@gmail.com>
2025-10-22 23:05:41 +0300

    Add files via upload

---
+++ conditional.js
@@ -0,0 +1,9 @@
+let score = 75;
+
+if (score >= 90) {
+  console.log("You got an A! 🎉");
+} else if (score >= 70) {
+  console.log("You got a B! Keep up the good work."); // This line will execute
+} else {
+  console.log("You got a C or lower. Study harder.");
+}
