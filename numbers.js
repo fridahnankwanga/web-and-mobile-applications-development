95a1d16353ebc281a6756c99b4d83da5d7041515
fridahnankwanga <fridahnankwanga@gmail.com>
2025-10-22 23:05:11 +0300

    Add files via upload

---
+++ numbers.js
@@ -0,0 +1,10 @@
+// 1. Declare two variables
+let num1 = 10;
+const num2 = 5;
+
+// 2. Perform a calculation
+let sum = num1 + num2;
+
+// 3. Print the result to the console
+console.log("The first number is: " + num1); // Output: The first number is: 10
+console.log("The sum is: " + sum);          // Output: The sum is: 15
